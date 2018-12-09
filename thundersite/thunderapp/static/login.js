d9d6aa8909c3b62cae31153ac613370124af8ab7
Stephen Gooch <ec16473@qmul.ac.uk>
2018-12-09 12:29:55 +0000

    stephenbugfixes and redirect on login + register

---
+++ thundersite/thunderapp/static/login.js
@@ -0,0 +1,56 @@
+
+
+var input = document.getElementById("inputPassword");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+        if (event.keyCode === 13) {
+            // Trigger the button element with a click
+            loginUser()
+        }
+});
+
+var input2 = document.getElementById("inputUserName");
+
+// Execute a function when the user releases a key on the keyboard
+input2.addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+    if (event.keyCode === 13) {
+        // Trigger the button element with a click
+        loginUser()
+    }
+});
+
+
+
+function loginUser() {
+
+    $.ajax({
+        type: "POST",
+        url:'/login/',
+        data:  {
+            'username' : $('#inputUserName').val(),
+            'password':$('#inputPassword').val(),
+            'csrfmiddlewaretoken':$("input[name=csrfmiddlewaretoken]").val()
+        },
+        success: function(response) {
+
+
+            if (response.success ==false) {
+
+                $("#errorLoginAlert").show();
+                $("#errorLoginAlert").delay(1500).addClass("in").fadeOut(2000);
+            }
+            else {
+                $("#successLoginAlert").show();
+                $("#successLoginAlert").delay(700).addClass("in").fadeOut(2000);
+                console.log("LOGIN SUCCESSFUL");
+                setTimeout(function () {
+                    window.location = '/profile/'
+                }, 700);
+            }
+        }
+    });
+}
+
